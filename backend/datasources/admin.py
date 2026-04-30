651e07304956f543284bfff1d085bb012c1e1d6a
Tonghe Zhou <zthup2021@outlook.com>
2026-05-01 00:14:21 +0200

    Implement backend data sources for projects

---
+++ backend/datasources/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import DataSource
+
+
+@admin.register(DataSource)
+class DataSourceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "project",
+        "source_type",
+        "data_format",
+        "contains_personal_data",
+        "created_at",
+    )
+    list_filter = (
+        "source_type",
+        "data_format",
+        "contains_personal_data",
+        "project",
+    )
+    search_fields = ("name", "description", "location", "project__name")
