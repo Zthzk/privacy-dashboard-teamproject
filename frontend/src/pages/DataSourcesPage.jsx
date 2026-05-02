d3808fcff6502d0604708e39cd401a2bee5a9a85
Vadim <vadim.sawarski@student.uni-tuebingen.de>
2026-05-02 16:48:54 +0200

    added frontend logic for add-dataset

---
+++ frontend/src/pages/DataSourcesPage.jsx
@@ -0,0 +1,15 @@
+/**
+ * Page for viewing and adding data sources for a project.
+ */
+import DataSourceList from "../components/DataSourceList";
+
+function DataSourcesPage() {
+  return (
+    <main>
+      <h1>Data Sources</h1>
+      <DataSourceList />
+    </main>
+  );
+}
+
+export default DataSourcesPage;
